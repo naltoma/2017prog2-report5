8b12a60d7ad9289c6927978ea7b5c007fd33ad8f
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2018-01-09 20:55:29 +0900

    first commit

---
+++ src/main/java/jp/ac/uryukyu/ie/tnal/Enemy.java
@@ -0,0 +1,59 @@
+package jp.ac.uryukyu.ie.tnal;
+
+/**
+ * 敵クラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Enemy extends LivingThing {
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    @Override
+    public void wounded(int damage){
+        updateHitPoint(damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+
+    /**
+     * opponentへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
+    @Override
+    public void attack(LivingThing opponent){
+        if( isDead() == false ) {
+            int damage = (int) (Math.random() * getAttack());
+            if ( damage == 0 ) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            }else {
+                if( Math.random() < 0.3 ){
+                    damage *= 2;
+                    System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                }else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                }
+                opponent.wounded(damage);
+            }
+        }
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/tnal/Hero.java
@@ -0,0 +1,60 @@
+package jp.ac.uryukyu.ie.tnal;
+
+/**
+ * ヒーロークラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Hero extends LivingThing {
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+    public Hero (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    @Override
+    public void wounded(int damage){
+        updateHitPoint(damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+        }
+    }
+
+    /**
+     * opponentへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
+    @Override
+    public void attack(LivingThing opponent){
+        if( isDead() == false ) {
+            int damage = (int) (Math.random() * getAttack());
+            if ( damage == 0 ) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            }else {
+                if( Math.random() < 0.4 ){
+                    damage *= 2;
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                }else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                }
+                opponent.wounded(damage);
+            }
+        }
+    }
+
+}
